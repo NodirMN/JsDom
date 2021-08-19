a987c28cb3ed2be2752597489e26c72d448bd8e0
@NodirMN <998946140194@mail.ru>
2021-08-18 14:46:39 -1200

    Update Dom.js

--- Dom.js
+++ Dom.js
@@ -1,3 +1,22 @@
 let a = document.querySelector('button')
+
+///style
+
 a.style.color= 'red'
+a.style.border= 0
+
+////text
+
+a.textContent = 'Order a Call' //text qib beradi
+
+
+//////tag yozish
+
+a.innerHTML = '<i>Qo`ng`iroqqa buyurtma berish<i>' // tipa htmlda beradi
+
+///////class
+
+a.classList.add('show')  // class qo'shish
+console.log(a.classList.contains('show')); // tekshirsa bo'laadi show class bormi
+
 console.log(a);
